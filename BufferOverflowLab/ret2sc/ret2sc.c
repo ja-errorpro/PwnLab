569d5e10f6fc5c3efcb812b1b7868991b415d429
ja-errorpro <u810025+Git@gmail.com>
2024-05-03 19:47:17 +0800

    Let .bss executable

--- BufferOverflowLab/ret2sc/ret2sc.c
+++ BufferOverflowLab/ret2sc/ret2sc.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 char name[50];
 
-int main(){
+void initchal(){
+	setvbuf(stdin,0,2,0);
 	setvbuf(stdout,0,2,0);
+	unsigned long addr = (unsigned long)&name & ~0xfff;
+	mprotect((void *)addr, 0x1000, PROT_EXEC | PROT_READ | PROT_WRITE);
+}
+
+int main(){
+	initchal();
 	puts("Input your name: ");
 	read(0, name, 50);
 	char str[20];
